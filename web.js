51d4570367604f97de24a05e1e8a8d50cb14135b
karthic aragam <karagam123@gmail.com>
2013-04-15 20:40:36 -0400

    beginning

--- web.js
+++ web.js
@@ -1,12 +1,39 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+// Express initialization
+var express = require('express');
+var app = express(express.logger());
+app.use(express.bodyParser());
+app.set('title', 'nodeapp');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+// Mongo initialization
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/scorecenter';
+var mongo = require('mongodb');
+var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+app.post
+
+app.get('/', function (request, response) {
+	/*
+		db.collection('NAME_OF_YOUR_COLLECTON_HERE...', function(er, collection) {
+			collection.find()...
+	*/
+	response.set('Content-Type', 'text/html');
+	response.send('<p>Hi!</p>');
 });
+
+app.get('/data.json', function(request, response) {
+	response.set('Content-Type', 'text/json');
+	response.send('{"status":"good"}');
+});
+
+app.get('/fool', function(request, response) {
+	response.set('Content-Type', 'text/html');
+	response.send(500, 'Something broke!');
+});
+
+// Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
+app.listen(process.env.PORT || 3000);
